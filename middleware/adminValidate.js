32010efb5c1a4463de5f4a645e1f91b866cb0eed
priyadarshi4 <priyadarshiprince5@gmail.com>
2024-10-25 03:22:55 +0530

    Add files via upload

---
+++ middleware/adminValidate.js
@@ -0,0 +1,24 @@
+
+const jwt = require("jsonwebtoken")
+require("dotenv").config();
+
+ async function validateAdmin(req,res,next){
+    try{
+        let token = req.cookies.token;
+        if(!token) return res.send("you need to login first");
+
+        let data = await jwt.verify(token,process.env.JWT_KEY);
+        req.user = data ;
+
+        next();
+    }
+    catch(err){
+        res.send(err)
+    }
+}
+async function userIsLoggedIn(req,res,next){
+    if(req.isAuthenticated()) return next();
+    res.redirect("/users")
+}
+
+module.exports = {validateAdmin , userIsLoggedIn};
